45c3e9fbcb6c23ea986986f84811f544b58b5d26
Serena <rollohserena@gmail.com>
2024-01-15 03:46:45 -0800

    Task 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
-void _puts_recursion(char *s)
-/** 
- *main - run the function to print the string
+/**
+ * main -calls the function to print string
  *
  * Return: Always 0
+
  */
+void _puts_recursion(char *s)
 {
-while (*s != '\0') 
-{
-putchar(*s);
-s++;
-}
-putchar('\n');
-}
- int main(void){
-   _puts_recursion("I am an ALX student");
- return (0);
+    if (*s != '\0') {
+        putchar(*s);
+        _puts_recursion(s + 1);
+    } else {
+        putchar('\n');
+    }
 }
 
+int main(void)
+{
+    _puts_recursion("Hello, recursion!");
+    return 0;
+}
