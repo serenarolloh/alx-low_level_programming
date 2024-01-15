0652bf765dc6e68464525486e0bd6899937c4fd1
Serena <rollohserena@gmail.com>
2024-01-15 03:38:53 -0800

    Task 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,11 @@
-#include <main.h>
+#include "main.h"
 
 void _puts_recursion(char *s)
+/** 
+ *main - run the function to print the string
+ *
+ * Return: Always 0
+ */
 {
 while (*s != '\0') 
 {
@@ -8,5 +13,9 @@ putchar(*s);
 s++;
 }
 putchar('\n');
+}
+ int main(void){
+   _puts_recursion("I am an ALX student");
+ return (0);
 }
 
